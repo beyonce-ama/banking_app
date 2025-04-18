c9ff758ea571315d16d65649f4883eb2b4dee654
bulanadi-jhonvianey <jhonvianeydelapenabulanadi04@gmail.com>
2025-04-18 17:24:14 +0800

    editted by jhonvianey

--- lib/dashboard_screen.dart
+++ lib/dashboard_screen.dart
@@ -1,6 +1,9 @@
-import 'login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+// Screens
+import 'login_screen.dart';
 import 'home_screen.dart';
 import 'transfer_screen.dart';
 import 'pay_bills_screen.dart';
@@ -19,13 +22,19 @@ class BankingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
-      home:  LoginScreen(),
+      theme: const CupertinoThemeData(
+        brightness: Brightness.light,
+        primaryColor: Color(0xFF3366FF),
+        barBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: Color(0xFFF8F9FA),
+      ),
+      home: const LoginScreen(),
     );
   }
 }
 
 class DashboardScreen extends StatefulWidget {
-   final User user;
+  final User user;
 
   const DashboardScreen({super.key, required this.user});
 
@@ -42,12 +51,13 @@ class _DashboardScreenState extends State<DashboardScreen> {
     super.initState();
     _pages = [
       HomeScreen(user: widget.user, onFeatureTap: _onItemTapped),
-      TransferPage(user: widget.user), 
+      TransferPage(user: widget.user),
       BillsPaymentPage(user: widget.user),
       CardDetailsPage(user: widget.user),
-       SettingsPage(user: widget.user),
+      SettingsPage(user: widget.user),
     ];
   }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -55,17 +65,17 @@ class _DashboardScreenState extends State<DashboardScreen> {
   }
 
   final List<IconData> _icons = [
-    CupertinoIcons.home,
-    CupertinoIcons.arrow_up_arrow_down,
-    CupertinoIcons.money_dollar,
-    CupertinoIcons.creditcard,
-    CupertinoIcons.settings,
+    Icons.home_rounded,
+    Icons.compare_arrows_rounded,
+    Icons.receipt_long_rounded,
+    Icons.credit_card_rounded,
+    Icons.settings_rounded,
   ];
 
   final List<String> _labels = [
     'Home',
     'Transfer',
-    'Pay Bills',
+    'Bills',
     'Cards',
     'Settings',
   ];
@@ -73,65 +83,80 @@ class _DashboardScreenState extends State<DashboardScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          _pages[_selectedIndex],
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Color(0xFF42A5F5),
-                    Color(0xFF1565C0),
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.15),
-                    blurRadius: 10,
-                    offset: const Offset(0, -2),
-                  ),
-                ],
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: List.generate(_icons.length, (index) {
-                  final isSelected = _selectedIndex == index;
-                  return GestureDetector(
-                    onTap: () => _onItemTapped(index),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(
-                          _icons[index],
-                          color: isSelected ? Colors.white : Colors.white70,
-                        ),
-                        const SizedBox(height: 4),
-                        Text(
-                          _labels[index],
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: isSelected ? Colors.white : Colors.white70,
-                          ),
-                        )
-                      ],
-                    ),
-                  );
-                }),
-              ),
-            ),
+      backgroundColor: const Color(0xFFF8FAFD),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: _buildBottomNavBar(),
+    );
+  }
+
+  Widget _buildBottomNavBar() {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.08),
+            blurRadius: 16,
+            offset: const Offset(0, -4),
           ),
         ],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(24),
+          topRight: Radius.circular(24),
+        ),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(_icons.length, (index) {
+            final isSelected = _selectedIndex == index;
+            return GestureDetector(
+              onTap: () => _onItemTapped(index),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                decoration: isSelected
+                    ? BoxDecoration(
+                        gradient: const LinearGradient(
+                          colors: [Color(0xFF3366FF), Color(0xFF6690FF)],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xFF3366FF).withOpacity(0.3),
+                            blurRadius: 10,
+                            offset: const Offset(0, 4),
+                          ),
+                        ],
+                      )
+                    : null,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      _icons[index],
+                      size: 24,
+                      color: isSelected ? Colors.white : Colors.grey[600],
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      _labels[index],
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                        color: isSelected ? Colors.white : Colors.grey[600],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
